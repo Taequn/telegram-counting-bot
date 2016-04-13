1f6ae50ed60e0325f4284f6fdbc3495b849bf029
Taequn <taequn@gmail.com>
2016-04-13 12:33:55 +0300

    Added files via upload

---
+++ SimpleBot.py
@@ -0,0 +1,64 @@
+from __future__ import division
+import graphlab
+graphlab.product_key.set_product_key('5849-266E-F22D-662F-0EF2-69DE-DC1E-69FB')
+from telegram.ext import Updater
+import logging
+
+logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+url = 'reading.csv'
+read = graphlab.SFrame.read_csv(url, header=True, column_type_hints=int)
+read_model = graphlab.linear_regression.create(read, target='Time',
+                                               features=['Words'],validation_set=None)
+
+def start(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Hello and welcome. All you need to do is to type a number of words.')
+
+
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, text="OK, just type a number, that's all")
+
+
+def echo(bot, update):
+    try:
+        print update.message.date
+        words = int(update.message.text)
+        longread = {'Words':[words]}
+        prep1 = read_model.predict(graphlab.SFrame(longread))
+        prep2 = graphlab.SArray(data=prep1, dtype=str)
+        prep3 = prep2/60
+        final = ''.join(str(e) for e in prep2)
+        final1 = ''.join(str(e) for e in prep3)
+        bot.sendMessage(update.message.chat_id, 'It will take %s seconds or %s minutes' % (final, final1))
+    except IndexError:
+        bot.sendMessage(update.message.chat_id, "Are you even listening? Give me numbers!")
+    except ValueError:
+        bot.sendMessage(update.message.chat_id, "Are you even listening? Give me numbers!")
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    updater = Updater("188108083:AAExpw65y9W-jTuE4oHaflbmmfzG7lbSpdc")
+
+    dp = updater.dispatcher
+
+    dp.addTelegramCommandHandler("start", start)
+    dp.addTelegramCommandHandler("help", help)
+
+    dp.addTelegramMessageHandler(echo)
+
+    dp.addErrorHandler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
